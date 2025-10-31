466ab9b211a338b3d33cb82e039f74e2c6ebc840
SHAHZAD777X <19241-sok@smgeducation.org>
2025-10-31 15:39:16 +0400

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,7 @@
+print("Hello welcom to class")
+print(40)
+print("sum of 123+23=",123+23)
+print("Bag cost is",250,end="$")
+
+
+
---
+++ a2.py
@@ -0,0 +1,9 @@
+x=100
+y="ibrahim"
+print("x= ",x)
+x=120
+print("x= ",x)
+print("y= ",y)
+
+age=int(input("enter your age:"))
+print("your age is:",age)
